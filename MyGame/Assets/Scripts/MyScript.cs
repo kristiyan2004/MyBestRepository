54932d4e4d011eb333f424b3964f9b1fd19fddf2
Kristiyan Hristov <dilarcheto1993k@abv.bg>
2023-05-28 23:03:28 +0300

    updated myscript

--- MyGame/Assets/Scripts/MyScript.cs
+++ MyGame/Assets/Scripts/MyScript.cs
@@ -7,7 +7,7 @@ public class MyScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Whatsup homie");
+        Debug.Log("Whatsup homie, I am so good at this game brother!!!");
     }
 
     // Update is called once per frame
